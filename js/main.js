25f31c837286bb5357410538e7255dee076f54c4
nakai <takayuki.nakai@nijibox.co.jp>
2018-04-09 16:33:54 +0900

    first commit to remote:made dir, files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+ * vueの特徴：双方向データバインディング（data <-> UI）
+ */
+(function(){
+    'use strict';
+    // UIに結びつくデータの設定
+    var vm = new Vue({
+        // 結合するUI
+        el: '#name',
+        // バインドする情報
+        data: {
+            name: ''
+        }
+    });
+
+    var app_vm = new Vue({
+        el: '#app',
+        data: {
+            todos: [
+                'task 1',
+                'task 2',
+                'task 3'
+            ]
+        }
+    });
+})();
