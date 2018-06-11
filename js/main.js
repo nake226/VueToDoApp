4e1f0cc69905f4dd23a90fd4da982148ccc64d33
中井　貴之 <uu135923@CA3665.local>
2018-06-11 18:51:18 +0900

    delete: extra html(just for lightning talk) modify: indent

--- js/main.js
+++ js/main.js
@@ -1,84 +1,66 @@
-/*
- * vueの特徴：双方向データバインディング（data <-> UI）
- */
-(function(){
-    'use strict';
-    var vm = new Vue({
-        // 結合する要素
-        el: '#app',
-        // データ
-        data: {
-            name: "",
-            task: "",
-            todos: [],
-            weight: '70'
-        },
-        // 監視
-        watch: {
-            /**
-             * @name todos
-             * タスクの増減のみを監視する
-             */
-            /*
-            todos: function(){
-                localStorage.setItem('todos', JSON.stringify(this.todos));
-                alert("Data saved!");
-            }
-            */
-            /**
-             * @name todos
-             * タスクの内部（プロパティ / 値）も監視する
-             */
-            todos: {
-                handler: function(){
-                    localStorage.setItem('todos', JSON.stringify(this.todos));
-                },
-                deep: true
-            }
-        },
-        // ライフサイクルフック
-        mounted: function(){
-            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-        },
-        // 関数
-        methods: {
-            // タスク追加
-            addTask(){
-                var newTask = {
-                    name: this.task,
-                    is_done: false
-                }
-                if (this.task.length !== 0) {
-                    this.todos.push(newTask); 
-                }
-                // todoの追加後に入力欄を空にする
-                this.task = "";
-            },
-            // タスク削除
-            deleteTask(index){
-                //if(confirm("you done?")){
-                    this.todos.splice(index, 1);
-                //}
-            },
-            // 処理済みタスクの削除
-            purge(){
-                if(!confirm('delete finished task?')){
-                    return;
-                }
-                this.todos = this.remaining;
-            }
-        },
-        // 算出プロパティ
-        computed: {
-            /**
-             * @name remaining
-             * @returns 残りのタスク
-             */
-            remaining: function(){
-                return this.todos.filter(function(todo){
-                    return !todo.is_done;
-                });
-            }
-        }
-    });
-})();
+'use strict';
+var vm = new Vue({
+  // 結合する要素
+  el: '#app',
+  // データ
+  data: {
+    name: "",
+    task: "",
+    todos: [],
+  },
+  // 監視
+  watch: {
+    /**
+     * @name todos
+     * タスクの内部（プロパティ / 値）も監視する
+     */
+    todos: {
+      handler: function(){
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      },
+      deep: true
+    }
+  },
+  // データがマウントされた直後
+  mounted: function(){
+    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+  },
+  // 関数
+  methods: {
+    // タスク追加
+    addTask(){
+      var newTask = {
+        name: this.task,
+        is_done: false
+      }
+      if (this.task.length !== 0) {
+        this.todos.push(newTask); 
+      }
+      // todoの追加後に入力欄を空にする
+      this.task = "";
+    },
+    // タスク削除
+    deleteTask(index){
+      this.todos.splice(index, 1);
+    },
+    // 処理済みタスクの削除
+    purge(){
+      if(!confirm('delete finished task?')){
+        return;
+      }
+      this.todos = this.remaining;
+    }
+  },
+  // 算出プロパティ
+  computed: {
+    /**
+     * @name remaining
+     * @returns 残りのタスク
+     */
+    remaining: function(){
+      return this.todos.filter(function(todo){
+        return !todo.is_done;
+      });
+    }
+  }
+});
